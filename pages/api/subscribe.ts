5f1166971f2a22cb29a5006d6dd375fcb05f7888
Seth McCullough <seth@endevrs.dev>
2023-01-27 08:36:58 -0500

    Setup subscribe api route

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -1,13 +1,35 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import sgClient from "@sendgrid/client";
+sgClient.setApiKey(process.env.SENDGRID_API_KEY!);
 
-type Data = {
-  name: string
-}
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method !== "PUT") {
+    res.setHeader("Allow", ["PUT"]);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+
+  try {
+    const { email } = req.body;
+
+    await sgClient.request({
+      url: "/v3/marketing/contacts",
+      method: "PUT",
+      body: {
+        contacts: [
+          {
+            email,
+          },
+        ],
+      },
+    });
+
+    return res.status(201).json({ message: "Success" });
+  } catch (error: any) {
+    console.error(error);
+    return res.status(500).json({ message: error?.message });
+  }
 }
